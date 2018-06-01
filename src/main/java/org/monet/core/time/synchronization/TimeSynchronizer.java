020f04b276fd2dde741792fce28278b10a6fe2fe
chenjuntao <chenjuntao@quancheng-ec.com>
2018-06-01 11:27:36 +0800

    handle ed25519

---
+++ src/main/java/org/monet/core/time/synchronization/TimeSynchronizer.java
@@ -0,0 +1,16 @@
+package org.monet.core.time.synchronization;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Synchronizes the network time with other nodes.
+ */
+public interface TimeSynchronizer {
+
+	/**
+	 * Synchronizes the network time with other nodes.
+	 *
+	 * @return The future.
+	 */
+	CompletableFuture<Void> synchronizeTime();
+}
